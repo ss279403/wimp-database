36358da6c78a98e2facaec9b3e3cfee847e1e5c1
Sam Spiers <sam.spiers@libertymutual.com>
2017-12-05 15:44:33 -0500

    Wimp Database

---
+++ src/main/java/com/lmig/gfc/wimp/models/Movie.java
@@ -0,0 +1,75 @@
+package com.lmig.gfc.wimp.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(length = 300)
+	private String title;
+	private Date releaseDate;
+	private Long budget;
+	@Column(length = 500, nullable = false)
+	private String distributor;
+
+	public Movie() {
+	}
+
+	public Movie(Long id, String title, Date releaseDate, Long budget, String distributor) {
+		this.id = id;
+		this.title = title;
+		this.releaseDate = releaseDate;
+		this.budget = budget;
+		this.distributor = distributor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public Long getBudget() {
+		return budget;
+	}
+
+	public void setBudget(Long budget) {
+		this.budget = budget;
+	}
+
+	public String getDistributor() {
+		return distributor;
+	}
+
+	public void setDistributor(String distributor) {
+		this.distributor = distributor;
+	}
+
+}
